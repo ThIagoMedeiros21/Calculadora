a84f6476ff0e80661a37f53592a930992ac1d738
ThiagoMedeiros21 <thfmadeiros01@gmail.com>
2024-12-31 14:51:52 -0300

    Modificação na calculadora adicionando função

--- main.c
+++ main.c
@@ -1,66 +1,81 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main(){
-    int option;
-    
-    printf("SEJA BEM VINDO A SUA CALCULADORA\n DIGITE:\n 1-CALCULADORA SIMPLES\n 2-CALCULADORA DE MED ");
-    scanf("%d",&option);
-    if(option==1){
-        float resultado;
-        int val1,val2;
-        char op;
-
-        printf("DIGITE O PRIMEIRO NUMERO: ");
-        scanf("%d",&val1);
-        printf("DIGITE O SEGUNDO NUMERO: ");
-        scanf("%d",&val2);
-        printf("DIGITE O SINAL DO OPERADOR(+,-,*,/) ");
-        scanf(" %c",&op);
-        if(op=='+'){
-            resultado=val1+val2;
-        }
-        else if(op=='-'){
-            resultado=val1-val2;
-        }
-        else if(op=='*'){
-            resultado=val1*val2;
-        }
-        else if(op=='/'){
-            if(val2!=0){
-                resultado=val1/val2;
-            
-            }
-
-            else{
-                printf("Divisao por 0\n");
-            }
-        }
-        printf("RESULTADO: %f",resultado);
-
-        }
-
-    else if(option==2){
-        int n;
+    int soma(int a, int b){
+        int sum;
+        sum=a+b;
+        return sum;
         
-        printf("DIGITE A QUANTIDADE DE VALORES ");
-        scanf("%d",&n);
-        int numeros[n];
-        float med,soma=0.0;
-        for(int i=0;i<n;i++){
-            printf("DIGITE OS VALORES ");
-            scanf("%d",&numeros[i]);
-            
-        }
-
-        for(int c=0;c<n;c++){
-            soma+=numeros[c];
-            med=soma/n;
-            
-        }
-
-        printf("SUA MEDIA FOI: %f",med);
-    
     }
-    return 0;
 
-}
+    int subtracao(int a, int b){
+        int sub;
+        sub=a-b;
+        return sub;
+    }
+
+    int mult(int a, int b){
+        int multi;
+        multi=a*b;
+        return multi;
+    }
+    
+    float divi(int a, int b){ 
+    float divisao;
+    divisao=(float)a/b; 
+    return divisao;
+    }
+
+    int main(){
+        int option;
+        
+        printf("SEJA BEM VINDO A SUA CALCULADORA\n DIGITE:\n 1-CALCULADORA SIMPLES\n 2-CALCULADORA DE MED ");
+        scanf("%d",&option);
+        if(option==1){
+            int val1,val2;
+            int op;
+            
+            printf("DIGITE O PRIMEIRO NUMERO: ");
+            scanf("%d",&val1);
+            printf("DIGITE O SEGUNDO NUMERO: ");
+            scanf("%d",&val2);
+            printf("DIGITE:\n 1-SOMA\n 2-SUBTRAÇÃO\n 3-MULTIPLICAÇÃO\n 4-DIVISÃO");
+            scanf(" %d",&op);
+            
+            if(op==1){
+                printf("%d",soma(val1,val2));
+                
+            }
+            else if(op==2){
+                printf("%d",subtracao(val1,val2));
+            }
+            else if(op==3){
+                printf("%d", mult(val1,val2));
+            }
+            else if (op == 4) {
+            if (val2 != 0) {
+                printf("RESULTADO: %f\n", divi(val1, val2));
+            } else {
+                printf("DIVISÃO POR 0\n");
+            }
+            }
+        }
+        else if(option==2){
+            int n;
+            printf("DIGITE A QUANTIDADE DE VALORES ");
+            scanf("%d",&n);
+            int numeros[n];
+            float med,soma=0.0;
+            for(int i=0;i<n;i++){
+                printf("DIGITE OS VALORES ");
+                scanf("%d",&numeros[i]);
+            }
+
+            for(int c=0;c<n;c++){
+                soma+=numeros[c];
+                med=soma/n;
+            }
+            printf("SUA MEDIA FOI: %f",med);
+            }
+        return 0;
+        }
+    
